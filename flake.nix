86996473cbde564bc77eb831535740ec98976ebf
Brian Ryall <brian@brianryall.xyz>
2021-12-13 16:31:50 -0500

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "polarmutex customized dwm";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+
+      overlay = final: prev: {
+        dwm = prev.dwm.overrideAttrs (old: {
+          src = builtins.path { path = ./.; name = "dwm"; };
+        });
+      };
+    in
+    {
+      inherit overlay;
+
+      checks.${system}.build = (
+        import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        }
+      ).dwm;
+    };
+}
